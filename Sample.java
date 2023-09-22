b8ef7e40b817c8b0ffe55d2a1c8970aad9b48607
Angel-777333 <p.angelsaran@gmail.com>
2023-09-22 13:11:44 +0530

    first commit

---
+++ Sample.java
@@ -0,0 +1,17 @@
+package com.selenium.concept;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\vellar church\\eclipse-workspace\\Selenium\\Driver1.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com");
+	driver.manage().window().maximize();
+	String c = driver.getTitle();
+	System.out.println(c);
+	}
+
+}
